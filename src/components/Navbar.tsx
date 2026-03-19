58233fc86f9b62714e91b185ba376c28a1284b22
NITHISHKRISHNA-RAMAKRISHNAN <nithishkrishnaramakrishnan@gmail.com>
2026-03-19 16:27:47 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,7 +39,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between">
           <a href="#home" className="z-50">
-            <Logo variant={isScrolled ? "dark" : "light"} />
+            <img src="/logo.png" alt="Fovorse Logo" className="h-12 w-auto" />
           </a>
 
           {/* Desktop Menu */}
