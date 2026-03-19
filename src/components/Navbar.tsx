cf141d121ca9443134edd182d4d8fd5373b1f375
NITHISHKRISHNA-RAMAKRISHNAN <nithishkrishnaramakrishnan@gmail.com>
2026-03-19 16:07:19 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const links = [
   { name: "Home", path: "#home" },
   { name: "About", path: "#about" },
   { name: "Services", path: "#services" },
-  { name: "Framework", path: "#framework" },
+  { name: "Appoarch", path: "#Appoarch" },
   { name: "Results", path: "#results" },
   { name: "FAQ", path: "#faq" },
 ];
